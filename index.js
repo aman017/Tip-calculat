3db08aa3b89b4860cecd45c7e9a9ee7720d583e9
Aman Khanna <amankhanna017@gmail.com>
2019-09-14 01:28:33 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
-function calculate()
-{
-    var v=document.getElementById('rs').value;
-    var p=document.getElementById('per').value;
-     
-    var o=v*p/100;
-    
-    var m=document.getElementById('show').innerHTML=o;
+// eslint-disable-next-line no-unused-vars
+function calculate() {
+  var v = document.getElementById('rs').value
+  var perc = document.getElementById('per').value
+  var persons = document.getElementById('per').value
+  var o =
+    'Tip amount' +
+    '<br> <b>' +
+    Math.round((v * perc) / 100 / persons) +
+    '</b>' +
+    ' rs per person'
+
+  var m = (document.getElementById('show').innerHTML = o)
 }
